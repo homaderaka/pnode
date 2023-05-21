02dae74a2d60d57c9789f7e24aed5f489ce285af
ecookie <gladiolud64@gmail.com>
2023-05-21 23:36:09 +0300

    commands refactor

--- pkg/peerscmd/commands.go
+++ pkg/peerscmd/commands.go
@@ -1,30 +1,75 @@
 package peerscmd
 
 import (
+	"context"
 	"fmt"
+	"github.com/homaderaka/peersmsg"
 	"strings"
+
+	"pnode/internal/service" // Replace with your actual service package
 )
 
-type CommandHandler func(args []string) string
+type Command struct {
+	Args []string
+	Func CommandHandler
+}
 
-var CommandHandlers = map[string]CommandHandler{
-	"send": func(args []string) string {
-		if len(args) < 2 {
-			return "Error: send command expects 2 arguments"
-		}
-		// Do something with args[0] and args[1]
-		return fmt.Sprintf("Message sent to %s: %s", args[0], args[1])
-	},
-	"echo": func(args []string) string {
-		if len(args) == 0 {
-			return "Error: echo command expects at least 1 argument"
-		}
-		return strings.Join(args, " ")
-	},
-	"ping": func(args []string) string {
-		return "Pong!"
-	},
-	"trace": func(args []string) string {
-		return "Tracing..."
-	},
+type CommandHandler interface {
+	Execute(ctx context.Context, args []string) (string, error)
+}
+
+type SendCommand struct {
+	S *service.Service
+	P peersmsg.Parser
+}
+
+func (s SendCommand) Execute(ctx context.Context, args []string) (out string, err error) {
+	if len(args) < 1 {
+		return "", fmt.Errorf("Error: send command expects 1 argument")
+	}
+	// Here, you would normally send the message
+
+	message, err := s.P.FromString(strings.Join(args, " "))
+	if err != nil {
+		return
+	}
+
+	err = s.S.AddMessage(ctx, &message)
+	return fmt.Sprintf("Message sent: %v", args), nil
+}
+
+type GetCommand struct {
+	s *service.Service
+}
+
+func (g GetCommand) Execute(ctx context.Context, args []string) (string, error) {
+	messages, err := g.s.GetMessages(ctx)
+	if err != nil {
+		return "", err
+	}
+
+	derefStringsArray := make([]string, len(messages))
+	for i, strPtr := range messages {
+		derefStringsArray[i] = (*strPtr).String()
+	}
+
+	return strings.Join(derefStringsArray, "\n"), nil
+}
+
+type EchoCommand struct {
+}
+
+func (e EchoCommand) Execute(ctx context.Context, args []string) (string, error) {
+	if len(args) == 0 {
+		return "", fmt.Errorf("Error: echo command expects at least 1 argument")
+	}
+	return strings.Join(args, " "), nil
+}
+
+func NewCommandHandlers(s *service.Service, p peersmsg.Parser) map[string]CommandHandler {
+	return map[string]CommandHandler{
+		"send": SendCommand{s, p},
+		"get":  GetCommand{s},
+		"echo": EchoCommand{},
+	}
 }
