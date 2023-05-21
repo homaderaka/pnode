3dd9305e07723d512ce71392cb07d4d0d3d10979
ecookie <gladiolud64@gmail.com>
2023-05-21 14:11:53 +0300

    added storage

--- internal/storage/interface.go
+++ internal/storage/interface.go
@@ -1,1 +1,11 @@
 package storage
+
+import (
+	"context"
+	"github.com/homaderaka/peersmsg"
+)
+
+type Storage interface {
+	GetMessages(c context.Context) ([]*peersmsg.Message, error)
+	AddMessage(c context.Context, m *peersmsg.Message) error
+}
--- internal/storage/storage_ram.go
+++ internal/storage/storage_ram.go
@@ -1,3 +1,42 @@
 package storage
 
-typ
+import (
+	"context"
+	"github.com/homaderaka/peersmsg"
+	"sync"
+)
+
+type RAM struct {
+	Messages []*peersmsg.Message
+	M        sync.RWMutex
+}
+
+// GetMessages returns a slice of all stored messages.
+// Note: The returned messages should NOT be mutated.
+func (r *RAM) GetMessages(c context.Context) (m []*peersmsg.Message, err error) {
+	select {
+	case <-c.Done():
+		return nil, c.Err()
+	default:
+	}
+
+	r.M.RLock()
+	defer r.M.RUnlock()
+
+	m = r.Messages
+	return
+}
+
+func (r *RAM) AddMessage(c context.Context, m *peersmsg.Message) (err error) {
+	select {
+	case <-c.Done():
+		return c.Err()
+	default:
+	}
+
+	r.M.Lock()
+	defer r.M.Unlock()
+
+	r.Messages = append(r.Messages, m)
+	return
+}
